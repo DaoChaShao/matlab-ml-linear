d75bb4c8e83ab16143f372b21d18f2253bb8ddc1
DaoChaShao <27882272@qq.com>
2025-09-20 00:26:59 +0800

    feat: add a enum with off and on

---
+++ +helper/OffOn.m
@@ -0,0 +1,15 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : OffOn.m
+% Author      : daochashao
+% Date        : 2025-09-19 17:21:45
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+classdef OffOn
+    enumeration
+        Off
+        On
+    end
+end
